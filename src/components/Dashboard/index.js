fdcf08276d208b2cf689bdb8f612830d54cdbf2f
LemonLim18 <limmiinning@gmail.com>
2024-07-06 19:32:51 +0800

    Delete the employee from the firebase and reflect the latest data to the dashboard

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -7,7 +7,7 @@ import Add from './Add';
 import Edit from './Edit';
 
 // collection is the table; getDocs is the query for records in the table
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, doc, deleteDoc } from "firebase/firestore";
 import { db } from '../../config/firestore';
 
 // import the employee data from the local storage
@@ -33,6 +33,7 @@ const Dashboard = ({ setIsAuthenticated }) => {
     // querySnapshot.docs is an array of documents applicable, the properties and values are available but needed to be called
     // In this case, map creates a new array based on the filtered data 
     // Also, we declare a new property called "id" and use the existing properties from ...doc.data & copy its values
+    // Imagine ...doc.data() as the complete whole records
     const employees = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data()}));
     console.log("Employee Information: ", employees)
     setEmployees(employees);
@@ -50,7 +51,7 @@ const Dashboard = ({ setIsAuthenticated }) => {
     setIsEditing(true);
   };
 
-  const handleDelete = id => {
+  const handleDelete = (id) => {
     Swal.fire({
       icon: 'warning',
       title: 'Are you sure?',
@@ -58,22 +59,40 @@ const Dashboard = ({ setIsAuthenticated }) => {
       showCancelButton: true,
       confirmButtonText: 'Yes, delete it!',
       cancelButtonText: 'No, cancel!',
-    }).then(result => {
+    }).then(
+      async (result) => {
       if (result.value) {
+        // filter returns an array, but since we'll only have one object, so we use [] to destructure
         const [employee] = employees.filter(employee => employee.id === id);
-
+        // console.log(employee) // I will get an object/element
         // TODO delete document
-
-        Swal.fire({
-          icon: 'success',
-          title: 'Deleted!',
-          text: `${employee.firstName} ${employee.lastName}'s data has been deleted.`,
-          showConfirmButton: false,
-          timer: 1500,
-        });
-
-        const employeesCopy = employees.filter(employee => employee.id !== id);
-        setEmployees(employeesCopy);
+        try {
+          console.log("Deleting employee: ", employee.id, "with the name: ", employee.firstName, employee.lastName)
+          // Create a reference to the document to be deleted
+          const deleteRef = doc(db, "employees", employee.id);
+          
+          // Delete the document
+          await deleteDoc(deleteRef);
+          console.log("Document successfully deleted!");
+          Swal.fire({
+            icon: 'success',
+            title: 'Deleted!',
+            text: `${employee.firstName} ${employee.lastName}'s data has been deleted.`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+          // filtered out those not related employee records(remainder)
+          const employeesCopy = employees.filter(employee => employee.id !== id);
+          setEmployees(employeesCopy);  // update the local state with the remaining employee records
+        } catch (error) {
+          console.log("Error deleting document: ", error);
+          Swal.fire({
+            icon: 'error',
+            title: 'Error!',
+            text: 'Failed to delete employee data.',
+            showConfirmButton: true,
+          });
+        }
       }
     });
   };
